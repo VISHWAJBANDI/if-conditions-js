e91171e1c3490318fe8e277fe7455fca8b9832af
VISHWAJBANDI <126488543+VISHWAJBANDI@users.noreply.github.com>
2024-10-22 10:47:11 +0530

    Add files via upload

---
+++ if.js
@@ -0,0 +1,31 @@
+var age=25;
+if( age >= 18 && age < 18){
+    console.log("your are eligible for vote")
+}else{
+    console.log("you are not eligible for vote")
+}
+
+var Weather= "rainy";
+if(Weather == "rainy"){
+    console.log("its raining outside")
+}else{
+    console.log("not raining")
+}
+
+let marks=89;
+if( marks > 90 && marks > 90){
+    console.log("A+")
+}else if( marks >= 81 && marks <= 90){
+    console.log("A")
+}else if( marks >= 71 && marks <= 80){
+    console.log("B+")
+}else if( marks >= 61 && marks <= 70){
+    console.log("C+")
+}else if( marks >= 51 && marks <= 60){
+    console.log("C")
+}else if( marks >= 41 && marks <= 50){
+    console.log("D")
+}else{
+    console.log("fail")
+}
+    
